6c978a70dea8c8443431dd748928959e05bd1374
Ole Bøe Andreassen <ole.boe.andreassen@nhn.no>
2022-01-10 16:09:00 +0100

    quick update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@
 # Update and upgrade and install homebrew dependencies
 sudo apt update && sudo apt upgrade -y && sudo apt install whiptail build-essential -y
 # Set up private passwordless key
-ssh-keygen -t ed25519 -f "$HOME/.ssh/nopass_ed25519" -q -N ""
+mkdir -p "$HOME/.ssh"
+touch "$HOME/.ssh/config"
+
+SSH_KEY_FILE="nopass_ed25519"
+ssh-keygen -t ed25519 -f "$HOME/.ssh/$SSH_KEY_FILE" -q -N ""
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -15,15 +19,20 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 # Set up config from git or local config folder
 mkdir -p "$HOME/.config/shell-config"
 
-COLOR=$(whiptail --inputbox "Type in github config repo" 8 39 --title "Example Dialog" 3>&1 1>&2 2>&3)
+REPO=$(whiptail --inputbox "Type in github config repo" USER/REPO 8 39 --title "Example Dialog" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
+    echo "Host github.com" >> "$HOME/.ssh/config" 
+    echo "  IdentityFile ~/.ssh/$SSH_KEY_FILE" >> "$HOME/.ssh/config" 
+    echo "  IdentitiesOnly yes" >> "$HOME/.ssh/config" 
+    echo "" >> "$HOME/.ssh/config" 
+
     brew install gh
     gh auth login
-    gh ssh-key add "$HOME/.ssh/nopass_ed25519.pub"
-    gh repo clone $COLOR
+    gh ssh-key add "$HOME/.ssh/$SSH_KEY_FILE.pub"
+    gh repo clone $REPO "$HOME/.config/shell-config"
 else
-    echo "User selected Cancel."
+    mkdir -p "$HOME/.config/shell-config"
 fi
 
 # Install from Brewfile
--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
-
-COLOR=$(whiptail --inputbox "Type in github config repo" 8 39 --title "Example Dialog" 3>&1 1>&2 2>&3)
+REPO=$(whiptail --inputbox "Type in github config repo" USER/REPO 8 39 --title "Example Dialog" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered " $COLOR
+    echo "Host github.com" >> "$HOME/.ssh/config" 
+    echo "  IdentityFile ~/.ssh/$SSH_KEY_FILE" >> "$HOME/.ssh/config" 
+    echo "  IdentitiesOnly yes" >> "$HOME/.ssh/config" 
+    echo "" >> "$HOME/.ssh/config" 
+
+    brew install gh
+    gh auth login
+    gh ssh-key add "$HOME/.ssh/$SSH_KEY_FILE.pub"
+    gh repo clone $REPO "$HOME/.config/shell-config"
 else
-    echo "User selected Cancel."
+    mkdir -p "$HOME/.config/shell-config"
 fi
